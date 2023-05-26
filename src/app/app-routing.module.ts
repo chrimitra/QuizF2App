08f51b77789ee723faeb7e0f419c0d1fc415f7c4
Zir0Code <edoardoalessandro@gmail.com>
2023-05-26 09:13:04 +0200

    creato il service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { QuizComponent } from './components/quiz/quiz.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent }
+  { path: '', component: LoginComponent },
+  { path: 'quiz', component: QuizComponent },
+  { path: '*', component: QuizComponent }
 ];
 
 @NgModule({
