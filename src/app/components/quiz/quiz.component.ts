282cd6d0ecd6b4603974352e0ea5f2928350c6b4
Zir0Code <edoardoalessandro@gmail.com>
2023-05-26 10:53:50 +0200

    preimpostate le logiche per le chiamate a dynamo

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class QuizComponent {
 
   x : number =0;
-  domandeJson : any[] = [
+  domandeJson : any[] = [    //da definire nell oninit tramite chiamataa dynamo *-*-*-*-*-*-*-*-*-*-*-*-*-*
     {
     domanda:"cosa beve spesso Edoardo?",
     risposte:["acqua","fanta","spremuta di carciofo","Monster"]},
@@ -19,24 +19,70 @@ export class QuizComponent {
 ]
   risposteCorrette:String[] = [];
 
+  risposteDate:any[] = [];
+
 ngOnInit(): void {
+  //salviamo le risposte giusta(per ora sempre nell'ultima posizione) in un array
   for(let i =0; i<this.domandeJson.length;i++){
     this.risposteCorrette.push(this.domandeJson[i].risposte[3])
   }
- console.log(this.risposteCorrette)
+  //mischiamo le risposte per il quiz
+ for(let i =0; i<this.domandeJson.length;i++){
+  this.shuffle(this.domandeJson[i].risposte)
+}
+
 }
 
 
-  avanti(risposta:any){
-
-
-    if(risposta == this.risposteCorrette[this.x]){
-      console.log("BRAVO")
-    }else{
-      console.log("risposta sbagliata")
+  avanti(rispostaInv:any,domandaInv:string){
+    let rispostaJson ={
+      domanda:domandaInv,
+      risposta : rispostaInv,
+      verifica : ""
     }
 
+    //verifichiamo se la risposta è corretta o no
+    if(rispostaInv == this.risposteCorrette[this.x]){
+      rispostaJson.verifica ="Risposta giusta"
+    }else{
+      rispostaJson.verifica ="Risposta sbagliata"
+    }
+    //inseriamo il tutto nella lista di risposte dell'utente
+    this.risposteDate.push(rispostaJson)
+
+
+    //per mandare avanti le domande
     if(this.x!= this.domandeJson.length-1){
-    this.x++}else{this.x=0}
+    this.x++
+  }else{
+    //quando si finisce il quiz, si salva tutto e viene inviato a db
+      this.x=0;
+      let JsonInvio = {
+        utente:JSON.parse(sessionStorage.getItem("UtenteLoggato") as string),
+        risposte : this.risposteDate
+      }
+      //da aggiungere invio a db invece che settarlo nella session *-*-*-*-*-*-*-*-*-*-*-*-*-*
+      sessionStorage.setItem("provaInvio",JSON.stringify(JsonInvio))
+    }
   }
+
+
+
+
+
+  shuffle<T>(array: T[]): T[] {
+    let currentIndex = array.length,  randomIndex;
+    while (currentIndex != 0) {
+
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+    return array;
+  };
+
+
+
 }
