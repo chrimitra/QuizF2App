2564bb0a794d320c891c2a4420792f271df8999f
fiorafabio <108327824+fiorafabio@users.noreply.github.com>
2023-05-26 11:29:48 +0200

    getRandomQuestions

--- src/app/apis.service.ts
+++ src/app/apis.service.ts
@@ -2,15 +2,17 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApisService {
-
-  constructor(private http: HttpClient) {
-
+  constructor(private http: HttpClient) {}
+  putQuiz(data: any): Observable<any> {
+    console.log(data);
+    return this.http.post<any>('http://192.168.1.232:6050/putQuiz', data);
   }
- putQuiz(data: any): Observable<any> {
-   console.log(data)
-   return this.http.post<any>('http://192.168.1.232:6050/putQuiz', data);
- }
+  getRandomQuestions(questions: number): Observable<any[]> {
+    return this.http.get<any[]>(
+      'http://192.168.1.232:6050/getRandomQuestions/' + questions
+    );
+  }
 }
